d80d4ca099fcdfedd7a9e89c7ea8d8604f759e54
Uwe Dauernheim <djui@users.noreply.github.com>
2017-08-27 22:53:21 +0200

    [DEV-2455] Refactor import rule
    Instead of having indivdual sub-rule, just compare the given order with
    the expected order and return the first non-matching line.

--- rule/import/rule_test.go
+++ rule/import/rule_test.go
@@ -3,120 +3,83 @@
 package imports
 
 import (
+	"bufio"
+	"bytes"
 	"go/parser"
 	"go/token"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestProcessImportsSuccess(t *testing.T) {
-	fset := token.NewFileSet()
-	source := `
-	package main
+var testcases = []struct {
+	name     string
+	given    string
+	expected string
+}{
+	{
+		name: "valid",
+		given: `
+				"flag"
+				"log"
+				"os"
+				"path/filepath"
+				"strings"
+				`,
+		expected: "",
+	},
+	{
+		name: "unsortedStdlib",
+		given: `
+				"flag"
+				"os"
+				"log"
+				"path/filepath"
+				"strings"
+				`,
+		expected: "main.go:5:5:error: incorrectly sorted import package: os",
+	},
+	{
+		name: "redundantGrouping",
+		given: `
+				"flag"
+				"log"
+				"os"
 
-	import (
-		"flag"
-		"log"
-		"os"
-		"path/filepath"
-		"strings"
-	)
-	func main() {
-		var b bytes.Buffer
-		b.Write([]byte("Hello "))
-		fmt.Fprintf(&b, "world!")
-	}`
-
-	f, err := parser.ParseFile(fset, "main.go", source, parser.ImportsOnly)
-	require.NoError(t, err)
-	hits := processImportRule(f, fset)
-
-	assert.Equal(t, 0, len(hits))
+				"path/filepath"
+				"strings"
+				`,
+		expected: "main.go:8:5:error: incorrectly sorted import package: path/filepath",
+	},
+	{
+		name: "stdlibAndOthersMixed",
+		given: `
+				"github.com/stretchr/testify/assert"
+				"flag"
+				"log"
+				"os"
+				"path/filepath"
+				"strings"
+				`,
+		expected: "main.go:4:5:error: incorrectly sorted import package: github.com/stretchr/testify/assert",
+	},
 }
 
-func TestProcessImportsWrongOrder(t *testing.T) {
-	fset := token.NewFileSet()
+func TestProcessImports(t *testing.T) {
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			fset := token.NewFileSet()
+			source := "package main\nimport (\n" + tc.given + ")\nfunc main() {}"
+			f, err := parser.ParseFile(fset, "main.go", source, parser.ImportsOnly)
+			require.NoError(t, err)
 
-	source := `
-	package main
-
-	import (
-		"flag"
-		"os"
-		"log"
-		"path/filepath"
-		"strings"
-	)
-	func main() {
-		var b bytes.Buffer
-		b.Write([]byte("Hello "))
-		fmt.Fprintf(&b, "world!")
-	}`
-
-	f, err := parser.ParseFile(fset, "main.go", source, parser.ImportsOnly)
-	require.NoError(t, err)
-
-	hits := processImportRule(f, fset)
-
-	assert.Equal(t, 1, len(hits))
-	assert.Equal(t, notSorted, hits[0].problem)
-}
-
-func TestProcessImportsWrongGrouping(t *testing.T) {
-	fset := token.NewFileSet()
-
-	source := `
-	package main
-
-	import (
-		"flag"
-		"log"
-		"os"
-
-		"path/filepath"
-		"strings"
-	)
-	func main() {
-		var b bytes.Buffer
-		b.Write([]byte("Hello "))
-		fmt.Fprintf(&b, "world!")
-	}`
-
-	f, err := parser.ParseFile(fset, "main.go", source, parser.ImportsOnly)
-	require.NoError(t, err)
-
-	hits := processImportRule(f, fset)
-
-	assert.Equal(t, 1, len(hits))
-	assert.Equal(t, notGrouped, hits[0].problem)
-}
-
-func TestProcessImportsWrongFirstPackage(t *testing.T) {
-	fset := token.NewFileSet()
-	source := `
-	package main
-
-	import (
-		"github.com/stretchr/testify/assert"
-		"flag"
-		"log"
-		"os"
-		"path/filepath"
-		"strings"
-	)
-	func main() {
-		var b bytes.Buffer
-		b.Write([]byte("Hello "))
-		fmt.Fprintf(&b, "world!")
-	}`
-
-	f, err := parser.ParseFile(fset, "main.go", source, parser.ImportsOnly)
-	require.NoError(t, err)
-
-	hits := processImportRule(f, fset)
-
-	assert.Equal(t, 1, len(hits))
-	assert.Equal(t, firstPackage, hits[0].problem)
+			var b bytes.Buffer
+			w := bufio.NewWriter(&b)
+			processImports(f, fset, w)
+			w.Flush()
+			assert.Equal(t, tc.expected, strings.TrimSpace(b.String()))
+		})
+	}
 }
