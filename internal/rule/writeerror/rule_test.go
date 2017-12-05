ddd854cc83bbcb5ca5a542872164762bd16cedd8
Uwe Dauernheim <djui@users.noreply.github.com>
2017-12-05 11:39:19 +0100

    Add test cases for writeerror rule

---
+++ internal/rule/writeerror/rule_test.go
@@ -0,0 +1,77 @@
+// Copyright (C) 2017 Betalo AB - All Rights Reserved
+
+package writeerror
+
+import (
+	"bufio"
+	"bytes"
+	"go/parser"
+	"go/token"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_findMissingReturn(t *testing.T) {
+	var cases = []struct {
+		name     string
+		given    string
+		expected string
+	}{
+		{
+			name: "Valid",
+			given: `
+				package main
+
+				import (
+					"errors"
+					"fmt"
+				)
+
+				func main() {
+					err := errors.New("dummy error")
+					if true {
+						rest.WriteError(w, r, err)
+						return
+					}
+					fmt.Println("dummy text")
+				}
+				`,
+			expected: "",
+		}, {
+			name: "Violation",
+			given: `
+				package main
+
+				import (
+					"errors"
+					"fmt"
+				)
+
+				func main() {
+					err := errors.New("dummy error")
+					if true {
+						writeError(w, r, err)
+					}
+					fmt.Println("dummy text")
+				}
+				`,
+			expected: "main.go:14:6: missing return statement after writeError call\n",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			fset := token.NewFileSet()
+			f, err := parser.ParseFile(fset, "main.go", tc.given, 0)
+			require.NoError(t, err)
+
+			var b bytes.Buffer
+			w := bufio.NewWriter(&b)
+			findMissingReturn(f, fset, w)
+			require.NoError(t, w.Flush())
+			assert.Equal(t, tc.expected, b.String())
+		})
+	}
+}
