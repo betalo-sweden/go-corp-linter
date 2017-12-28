ea80a2cf65dbdf0c50d2e5edfb934edb89af6289
Uwe Dauernheim <djui@users.noreply.github.com>
2017-12-28 19:08:12 +0100

    [DEV-3612] Add copyright linter

---
+++ internal/rule/copyright/rule.go
@@ -0,0 +1,58 @@
+// Copyright (C) 2017 Betalo AB - All Rights Reserved
+
+package copyright
+
+import (
+	"bytes"
+	"fmt"
+	"go/token"
+	"io"
+	"os"
+	"regexp"
+	"time"
+)
+
+var (
+	company                     = "Betalo AB"
+	copyrightHeaderPrefixRegexp = regexp.MustCompile(fmt.Sprintf(`// Copyright \([cC]\) .*%d %s - All Rights Reserved`, time.Now().Year(), company))
+)
+
+// ProcessFile checks for copyright headers in Go files.
+func ProcessFile(fp string, out io.Writer) error {
+	return findMissingCopyright(fp, out)
+}
+
+func findMissingCopyright(filepath string, out io.Writer) error {
+	b, err := head(filepath, 100) // Arbitrary, just has to be large enough
+	if err != nil {
+		return err
+	}
+
+	findMissingCopyrightInHeader(filepath, bytes.TrimSpace(b), out)
+	return nil
+}
+
+func findMissingCopyrightInHeader(filepath string, header []byte, out io.Writer) {
+	if !copyrightHeaderPrefixRegexp.Match(header) {
+		report(out, token.Position{Filename: filepath, Line: 1, Column: 1})
+	}
+}
+
+func head(filepath string, size int64) ([]byte, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = f.Close() }()
+
+	b := make([]byte, size)
+	_, err = f.Read(b)
+	if err != io.EOF {
+		err = nil
+	}
+	return b, err
+}
+
+func report(out io.Writer, position token.Position) {
+	fmt.Fprintf(out, "%s: Missing copyright header\n", position)
+}
